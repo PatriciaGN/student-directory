af6b8b22cad2c8e0276245c85870f223f6d82834
PattyFlowers <patty7flowers@gmail.com>
2022-09-04 16:36:43 +0000

    Change if statement to print students with name shorter than 10 chars

--- directory_copy.rb
+++ directory_copy.rb
@@ -35,7 +35,7 @@ end
 
 def print(students)
     students.each_with_index do |student, index|
-      if student[:name][0] == "D"
+      if student[:name].length < 10
          puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
       end
     end
