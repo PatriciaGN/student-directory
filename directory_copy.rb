bce594977b7a15dda9ca6de45e08be0e1ee6095a
PattyFlowers <patty7flowers@gmail.com>
2022-09-05 08:50:05 +0000

    Add input for cohort

--- directory_copy.rb
+++ directory_copy.rb
@@ -23,8 +23,21 @@ def input_students
     name= gets.chomp
     puts "Add the country of the student:"
     country = gets.chomp
+    cohort = ""
+    
+    while cohort.empty? do
+      puts "Add the cohort of the student:"
+      cohort = gets.chomp
+      if cohort == ""
+          cohort = "November"
+          break
+      elsif (cohort).capitalize != "January" and (cohort).capitalize != "February" and (cohort).capitalize != "March" and (cohort).capitalize != "April" and (cohort).capitalize != "May" and (cohort).capitalize != "June" and (cohort).capitalize != "July" and (cohort).capitalize != "August" and (cohort).capitalize != "September" and (cohort).capitalize != "October" and (cohort).capitalize != "November" and (cohort).capitalize != "December"
+        puts "Please, enter a valid cohort"
+        cohort = ""
+      end
+    end  
     while !name.empty? do
-        students << {name: name, cohort: :november, country: country}
+        students << {name: name, cohort: cohort, country: country}
         puts "Now we have #{students.count} students"
         puts "Add the name of the student:"
         name= gets.chomp
@@ -32,6 +45,18 @@ def input_students
           puts "Add the country of the student:"
           country = gets.chomp
         end
+    cohort = ""
+    while cohort.empty? do
+      puts "Add the cohort of the student:"
+      cohort = gets.chomp
+      if cohort == ""
+          cohort = "November"
+          break
+      elsif (cohort).capitalize != "January" and (cohort).capitalize != "February" and (cohort).capitalize != "March" and (cohort).capitalize != "April" and (cohort).capitalize != "May" and (cohort).capitalize != "June" and (cohort).capitalize != "July" and (cohort).capitalize != "August" and (cohort).capitalize != "September" and (cohort).capitalize != "October" and (cohort).capitalize != "November" and (cohort).capitalize != "December"
+        puts "Please, enter a valid cohort"
+        cohort = ""
+      end
+    end  
     end
     students
 end
