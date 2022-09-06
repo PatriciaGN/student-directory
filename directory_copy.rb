8e3be22bb60943c45e6be48f8a3fc104fb701e5c
PattyFlowers <patty7flowers@gmail.com>
2022-09-06 14:58:41 +0000

    Improve input_students method functionality

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,23 +1,8 @@
-=begin
-students = [
- {name: "Dr. Hannibal Lecter", cohort: :november},
- {name: "Darth Vader", cohort: :november},
- {name: "Nurse Ratched", cohort: :november},
- {name: "Michael Corleone", cohort: :november} ,
- {name: "Alex DeLarge", cohort: :november},
- {name: "The Wicked Witch of the West", cohort: :november},
- {name: "Terminator", cohort: :november},
- {name: "Freddy Krueger", cohort: :november},
- {name: "The Joker", cohort: :november},
- {name: "Joffrey Baratheon", cohort: :november},
- {name: "Norman Bates", cohort: :november}
-]
-=end
 @students = [] # An empty array accessible to all methods (instance variable)
+
 @cohorts = ["January", "February", "March", "April", 
   "June", "July", "August", "September", "October", "November", "December"
   ]
-
 
 def interactive_menu
   loop do
@@ -53,54 +38,33 @@ def show_students
 end
 
 def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-    puts "Add the name of the student:"
-    name= gets.gsub!("\n", "")
-    if name.empty?
-        puts "We don't have any students yet, would you like to join us? :)"
-        exit
-    end
-    puts "Add the country of the student:"
-    country = gets.chomp
-    cohort = ""
-    
-    while cohort.empty? do
-      puts "Add the cohort of the student:"
-      cohort = gets.chomp
-      if cohort == ""
-          cohort = "November"
-          break
-      elsif (cohort).capitalize != "January" and (cohort).capitalize != "February" and (cohort).capitalize != "March" and (cohort).capitalize != "April" and (cohort).capitalize != "May" and (cohort).capitalize != "June" and (cohort).capitalize != "July" and (cohort).capitalize != "August" and (cohort).capitalize != "September" and (cohort).capitalize != "October" and (cohort).capitalize != "November" and (cohort).capitalize != "December"
-        puts "Please, enter a valid cohort"
-        cohort = ""
-      end
-    end  
+    name = "Pepe"
     while !name.empty? do
-        @students << {name: name, cohort: cohort, country: country}
-        if @students.count == 1
-            puts "Now we have 1 student."
-        else
-            puts "Now we have #{@students.count} students."
-        end
-        puts "Add the name of the student:"
-        name= gets.chomp
-        if !name.empty?
-          puts "Add the country of the student:"
-          country = gets.chomp
-        end
-    cohort = ""
-        while cohort.empty? do
-      puts "Add the cohort of the student:"
+      puts "Please enter the names of the students"
+      puts "To finish, just hit return twice"
+      puts "Add the name of the student:"
+      name= gets.gsub!("\n", "")
+      if name.empty?
+        break
+      else
+      puts "Add the country of the student:"
+      country = gets.chomp
+      puts "Add the cohort of the student (if left blank, it will default to our November cohort):"
       cohort = gets.chomp
-      if cohort == ""
-          cohort = "November"
-          break
-      elsif !@cohorts.include?(cohort.capitalize)
-        puts "Please, enter a valid cohort"
-        cohort = ""
       end
-    end  
+      while !@cohorts.include?(cohort.capitalize) and cohort != ""
+        puts "Please, enter a valid cohort"
+        cohort = gets.chomp
+      end
+      if cohort == ""
+        cohort = "November"
+      end
+      @students << {name: name, cohort: cohort, country: country}
+      if @students.count == 1
+        puts "Now we have 1 student."
+      else
+        puts "Now we have #{@students.count} students."
+      end
     end
 end
 
