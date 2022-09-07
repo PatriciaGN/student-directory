f212d98950fda797e56f266b8fba5dc7019decf9
PattyFlowers <patty7flowers@gmail.com>
2022-09-07 08:51:21 +0000

    Add method to load students from students.csv and add to menu

--- directory.rb
+++ directory.rb
@@ -21,6 +21,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       return exit
     else
@@ -33,6 +35,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit"
 end
 
@@ -98,6 +101,15 @@ def save_students
         file.puts csv_line
     end
     file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, country, cohort = line.chomp.split(",")
+    @students << {name: name, country: country, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 interactive_menu
